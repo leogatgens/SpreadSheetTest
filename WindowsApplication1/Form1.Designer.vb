e0ec52ff24336bd06b6b1d4f06ec1914026cd7b5
Leonardo Meléndez <leogatgens@gmail.com>
2016-11-30 11:08:18 -0600

    Testing error formula on numericcelltype

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FpSpreadDesigner1 = New FarPoint.Win.Spread.Design.FpSpreadDesigner()
+        Me.FormulaProvider1 = New FarPoint.Win.Spread.FormulaProvider()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FormulaTextBox1 = New FarPoint.Win.Spread.FormulaTextBox()
+        Me.FpSpread1 = New FarPoint.Win.Spread.FpSpread()
+        Me.FpSpread1_Sheet1 = New FarPoint.Win.Spread.SheetView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.FormulaProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.FormulaTextBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.FpSpread1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(844, 660)
+        Me.SplitContainer1.SplitterDistance = 116
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(111, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Recalculate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 59)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "RecalculateALL"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FormulaTextBox1
+        '
+        Me.FormulaTextBox1.Location = New System.Drawing.Point(0, 88)
+        Me.FormulaTextBox1.Multiline = False
+        Me.FormulaTextBox1.Name = "FormulaTextBox1"
+        Me.FormulaTextBox1.Size = New System.Drawing.Size(796, 26)
+        Me.FormulaTextBox1.TabIndex = 5
+        'Attach FormulaTextBox1 to FpSpread1
+        Me.FormulaTextBox1.Attach(Me.FpSpread1)
+        '
+        'FpSpread1
+        '
+        Me.FpSpread1.AccessibleDescription = ""
+        Me.FpSpread1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FpSpread1.Location = New System.Drawing.Point(0, 0)
+        Me.FpSpread1.Name = "FpSpread1"
+        Me.FpSpread1.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.FpSpread1_Sheet1})
+        Me.FpSpread1.Size = New System.Drawing.Size(844, 540)
+        Me.FpSpread1.TabIndex = 4
+        '
+        'FpSpread1_Sheet1
+        '
+        Me.FpSpread1_Sheet1.Reset()
+        Me.FpSpread1_Sheet1.SheetName = "Sheet1"
+        'Formulas and custom names must be loaded with R1C1 reference style
+        Me.FpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1
+        Me.FpSpread1_Sheet1.ColumnFooterSheetCornerStyle.HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.General
+        Me.FpSpread1_Sheet1.ColumnFooterSheetCornerStyle.NoteIndicatorColor = System.Drawing.Color.Red
+        Me.FpSpread1_Sheet1.ColumnFooterSheetCornerStyle.Parent = "CornerFooterDefaultEnhanced"
+        Me.FpSpread1_Sheet1.ColumnFooterSheetCornerStyle.VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.General
+        Me.FpSpread1_Sheet1.FilterBarHeaderStyle.HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.General
+        Me.FpSpread1_Sheet1.FilterBarHeaderStyle.NoteIndicatorColor = System.Drawing.Color.Red
+        Me.FpSpread1_Sheet1.FilterBarHeaderStyle.Parent = "RowHeaderDefaultEnhanced"
+        Me.FpSpread1_Sheet1.FilterBarHeaderStyle.VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.General
+        Me.FpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(255, 58)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 660)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.FormulaProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FpSpreadDesigner1 As FarPoint.Win.Spread.Design.FpSpreadDesigner
+    Friend WithEvents FormulaProvider1 As FarPoint.Win.Spread.FormulaProvider
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents FormulaTextBox1 As FarPoint.Win.Spread.FormulaTextBox
+    Friend WithEvents FpSpread1 As FarPoint.Win.Spread.FpSpread
+    Friend WithEvents FpSpread1_Sheet1 As FarPoint.Win.Spread.SheetView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports FarPoint.Win
+Imports FarPoint.Win.Spread.CellType
+
+Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ConfigurarParametrosHoja()
+        ShowData()
+    End Sub
+
+    Private Sub ConfigurarParametrosHoja()
+        Me.FpSpread1.ActiveSheet.AutoCalculation = True
+        Me.FpSpread1.AllowUserFormulas = True
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FpSpread1.ActiveSheet.RecalculateAll()
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FpSpread1.ActiveSheet.Recalculate()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        FarPoint.Win.Spread.Design.ExternalDialogs.CellTypeEditor(Me.FpSpread1, FarPoint.Win.Spread.Design.DesignerMain.CellTypeEnum.CurrencyCellType)
+    End Sub
+    ''' <summary>
+    ''' When a Numeric cell has a formula and the result is !#Value the component is slower than before
+    ''' </summary>
+    Private Sub ShowData()
+        Dim cellTypeNumeric As New NumberCellType
+
+
+        FpSpread1.ActiveSheet.Cells(0, 1).CellType = cellTypeNumeric
+        'set formula
+        FpSpread1.ActiveSheet.Cells(0, 2).Formula = " a1 + b1"
+
+        FpSpread1.ActiveSheet.Cells(0, 3).Formula = " a1 + b1"
+        FpSpread1.ActiveSheet.Cells(0, 3).CellType = cellTypeNumeric 'Here is the problem  becuase the result  is on a numeric  cell
+        'set values
+        FpSpread1.ActiveSheet.Cells(0, 0).Text = "Hi string"
+        FpSpread1.ActiveSheet.Cells(0, 1).Value = 132
+    End Sub
+
+End Class
